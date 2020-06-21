fe15659e0595a408df944c3b87ada8952a7cfdfc
Arizona <geekwid@gmail.com>
2020-06-22 01:28:01 +0200

    post, patch and delete message

--- routes/message.js
+++ routes/message.js
@@ -6,17 +6,25 @@ const Message = require('../models/Message');
 router.get('/', async(req, res) => {
     try {
         const messages = await Message.find();
-        res.json(messages);
+        res.status(200).json(messages);
     } catch (err) {
-        res.json({ message: err });
+        res.status(500).json({ message: err });
     }
 });
 
 // Find all messages of a sender to a receiver
-router.get('/:authorId&:receiverId', (req, res) => {
-    console.log(req.params.authorId, req.params.receiverId);
-})
+router.get('/:authorId&:receiverId', async(req, res) => {
+    try {
+        const message = await Message.find({
+            authorId: req.params.authorId,
+            receiverId: req.params.receiverId
+        })
+        res.status(200).json(message);
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 
+})
 
 // Post a new message
 router.post('/', async(req, res) => {
@@ -29,10 +37,32 @@ router.post('/', async(req, res) => {
     });
     try {
         const savedMessage = await message.save()
-        res.json({ message: savedMessage, statusCode: res.status(200) });
+        res.status(201).json(savedMessage);
     } catch (err) {
-        res.json({ message: err, statusCode: res.status(500) });
+        res.status(500).json({ message: err });
     }
+});
+
+// Delete a message
+router.delete('/:messageId', async(req, res) => {
+    try {
+        const removedMessage = await Message.deleteOne({ _id: req.params.messageId });
+        res.status(200).json({ message: removedMessage });
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
+
+});
+
+// Update a message
+router.patch('/:messageId', async(req, res) => {
+    try {
+        const updatedMessage = await Message.updateOne({ _id: req.params.messageId }, { $set: { content: req.body.content, attachment: req.body.attachment, timestamp: req.body.timestamp } });
+        res.status(200).json({ message: updatedMessage });
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
+
 });
 
 module.exports = router;
